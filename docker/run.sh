865984f8bda301835d3fa3a6ca7ac673f1262d9a
lucasolives <lucasolives31@gmail.com>
2026-03-20 04:16:38 -0300

    fix: volumes and use of gpu

--- docker/run.sh
+++ docker/run.sh
@@ -10,12 +10,19 @@ PARENT_DIR="$(dirname -- "$SCRIPT_DIR")"
 echo "Diretório pai do script: $PARENT_DIR"
 
 # Comando para inicializar o contêiner Docker com bash
-rocker --device /dev/dri --x11 \
+# Força o GLX/OpenGL a usar o driver NVIDIA (evita llvmpipe/Mesa software no gzclient)
+rocker --devices /dev/dri --nvidia --x11 \
   --name $CONTAINER_NAME \
   --network host \
   --env ROS_IP=$ROS_IP \
   --env DISPLAY=$DISPLAY \
   --env XAUTHORITY=$XAUTHORITY \
-  --oyr-run-arg " -v $PARENT_DIR/src:/root/marta_bringup/src -v /tmp:/tmp -v /var/log:/var/log -v /tmp/.X11-unix:/tmp/.X11-unix" \
+  --env __GLX_VENDOR_LIBRARY_NAME=nvidia \
+  --env __NV_PRIME_RENDER_OFFLOAD=1 \
+  --volume "$PARENT_DIR/src:/root/marta_bringup/src" \
+  --volume /tmp:/tmp \
+  --volume /var/log:/var/log \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix \
+  -- \
   $IMAGE_NAME \
   ${@:-"bash"}
